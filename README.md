f136d90da0de64b8d4230f0baf9e7c935324ad4b
Rachel Marron <racheldmarron@gmail.com>
2022-03-30 19:10:35 -0700

    added user story, changed db

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # project2
+
+User Story:
+AS A user who participates in online shopping 
+I WANT an e-commerce site that shows me the sustainability rating and factory standards for clothes I want to buy
+SO THAT I can make conscious buying choices that help the planet
--- seeds/category-seeds.js
+++ seeds/category-seeds.js
@@ -2,19 +2,19 @@ const { Category } = require('../models');
 
 const categoryData = [
   {
-    category_name: 'Shirts',
+    category_name: 'Tops',
   },
   {
-    category_name: 'Shorts',
+    category_name: 'Bottoms',
   },
   {
-    category_name: 'Music',
-  },
-  {
-    category_name: 'Hats',
+    category_name: 'Swimwear',
   },
   {
     category_name: 'Shoes',
+  },
+  {
+    category_name: 'Accessories',
   },
 ];
 
