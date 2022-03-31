1844b290ac01ca2cda5fdbe9feb42a6730f970e2
Rachel Marron <racheldmarron@gmail.com>
2022-03-30 21:15:09 -0700

    updated seeds with relevant data, fixed env file

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -2,34 +2,34 @@ const { Product } = require('../models');
 
 const productData = [
   {
-    product_name: 'Plain T-Shirt',
-    price: 14.99,
-    stock: 14,
+    product_name: 'Brightside T-Shirt',
+    price: 78.00,
+    stock: 22,
     category_id: 1,
   },
   {
-    product_name: 'Running Sneakers',
-    price: 90.0,
-    stock: 25,
-    category_id: 5,
+    product_name: 'Terrycloth Shorts',
+    price: 55.0,
+    stock: 30,
+    category_id: 2,
   },
   {
-    product_name: 'Branded Baseball Hat',
-    price: 22.99,
-    stock: 12,
-    category_id: 4,
-  },
-  {
-    product_name: 'Top 40 Music Compilation Vinyl Record',
-    price: 12.99,
-    stock: 50,
+    product_name: 'Dune Bikini Set',
+    price: 108.70,
+    stock: 7,
     category_id: 3,
   },
   {
-    product_name: 'Cargo Shorts',
-    price: 29.99,
-    stock: 22,
-    category_id: 2,
+    product_name: 'Monterey Sneakers',
+    price: 127.00,
+    stock: 11,
+    category_id: 4,
+  },
+  {
+    product_name: 'Brixton Full Brim Hat',
+    price: 32.60,
+    stock: 18,
+    category_id: 5,
   },
 ];
 
