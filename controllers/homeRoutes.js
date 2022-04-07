b57c8e5a2e37bb0d1f61620c9de695a53b0e23ef
Tyler Welker <twelker08@gmail.com>
2022-04-07 19:30:26 -0400

    fixing links

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,6 +7,14 @@ router.get("/", async(req, res) => {
     res.render("landingpage");
 });
 
+router.get("/products", (req, res) => {
+    res.render("products")
+})
+
+router.get("/404", (req, res) => {
+    res.render("404")
+})
+
 //These routes help other routes redirect to /login with the auth middleware
 router.get("/login", (req, res) => {
     res.render("login");
